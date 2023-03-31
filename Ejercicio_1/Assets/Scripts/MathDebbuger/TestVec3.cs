82944cb978ed03fbf65cc13afb18f9c4edf1a765
ManuDant <manuel1000d@gmail.com>
2023-03-31 11:49:51 -0300

    Agregado test de las funciones ClampMagnitude y Magnitude

--- Ejercicio_1/Assets/Scripts/MathDebbuger/TestVec3.cs
+++ Ejercicio_1/Assets/Scripts/MathDebbuger/TestVec3.cs
@@ -9,6 +9,9 @@ public class TestVec3 : MonoBehaviour
     void Start()
     {
         TestAngle();
+        TestClampMagnitude();
+        TestMagnitude();
+        TestCross();
 
     }
 
@@ -24,4 +27,32 @@ public class TestVec3 : MonoBehaviour
         Debug.Log("El ángulo entre los vectores del Angle() en Unity es: " + angle2);
     }
 
+    private void TestClampMagnitude()
+    {
+        Vec3 vector = new Vec3(3f, 4f, 0f);
+
+        Vec3 clampedVector = Vec3.ClampMagnitude(vector, 2f);
+        Vector3 clampedVector2 = Vector3.ClampMagnitude(vector, 2f);
+
+        Debug.Log("Vector original: " + vector);
+        Debug.Log("Vector limitado propio: " + clampedVector);
+        Debug.Log("Vector limitado unity: " + clampedVector2);
+    }
+
+    private void TestMagnitude()
+    {
+        Vec3 vector = new Vec3(3f, 4f, 0f);
+
+        float magnitude = Vec3.Magnitude(vector);
+        float magnitude2 = Vector3.Magnitude(vector);
+
+        Debug.Log("Vector original: " + vector);
+        Debug.Log("Magnitud propio: " + magnitude);
+        Debug.Log("Magnitud en unity: " + magnitude2);
+    }
+
+    private void TestCross()
+    {
+
+    }
 }
